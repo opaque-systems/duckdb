04405ddbc3c1292a81585b330a581c5ccbba8b40
Ben Hoberman <ben@opaque.co>
2023-04-20 20:49:02 +0000

    Add Debug Build

--- generate_installs.sh
+++ generate_installs.sh
@@ -9,10 +9,10 @@ script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p "$script_dir/build"
 cd "$script_dir/build"
 mkdir -p "install"
-cmake -DCMAKE_INSTALL_PREFIX="install" ..
+cmake -GNinja -DCMAKE_INSTALL_PREFIX="install" ..
 
-make -j$(nproc)
-make install
+ninja -j$(nproc)
+ninja install
 
 # Go back to DuckDB main directory
 cd ..
@@ -21,10 +21,22 @@ cd ..
 mkdir -p "$script_dir/build_enclave"
 cd "$script_dir/build_enclave"
 mkdir -p "install"
-cmake -DCMAKE_INSTALL_PREFIX="install" -DLINK_OE=1 ..
+cmake -GNinja -DCMAKE_INSTALL_PREFIX="install" -DLINK_OE=1 ..
 
-make -j$(nproc)
-make install
+ninja -j$(nproc)
+ninja install
+
+# Go back to DuckDB main directory
+cd ..
+
+# Build and install enclave static library in debug mode
+mkdir -p "$script_dir/build_enclave_debug"
+cd "$script_dir/build_enclave_debug"
+mkdir -p "install"
+cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="install" -DLINK_OE=1 ..
+
+ninja -j$(nproc)
+ninja install
 
 # Go back to DuckDB main directory
 cd ..
@@ -34,5 +46,7 @@ cd build
 tar -zcf duckdb_install.tar.gz install
 cd ../build_enclave
 tar -zcf duckdb_enclave_install.tar.gz install
+cd ../build_enclave_debug
+tar -zcf duckdb_enclave_debug_install.tar.gz install
 cd ..
 
